6551bd6fb9f3d924f4e32f1385f19520c5ebc4e3
Christian Jaimes <christian.jaimes@biztory.be>
2024-07-26 15:06:21 +0000

    Intermediate CTE's added to the sql

--- models/legacy/fct_customer_orders.sql
+++ models/legacy/fct_customer_orders.sql
@@ -5,37 +5,40 @@ with
     customers as (select * from {{ source('jaffle_shop', 'customers') }}
     ),
 
-    payment as (select * from {{ source('stripe', 'payment') }}
+    payments as (select * from {{ source('stripe', 'payment') }}
     ),
 
-    paid_orders as (select orders.id as order_id,
-            orders.user_id    as customer_id,
+    paid_orders as (
+        select orders.id as order_id,
+            orders.user_id as customer_id,
             orders.order_date as order_placed_at,
-            orders.status     as order_status,
+            orders.status as order_status,
             p.total_amount_paid,
             p.payment_finalized_date,
-            c.first_name    as customer_first_name,
+            c.first_name as customer_first_name,
             c.last_name as customer_last_name
         from orders
-        
-        left join (select 
-                        orderid as order_id, 
-                        max(created) as payment_finalized_date, 
-                        sum(amount) / 100.0 as total_amount_paid
-                    from payment
-                    where status <> 'fail'
-                    group by 1) p on orders.id = p.order_id
-        left join customers c on orders.user_id = c.id )
+        left join (
+            select 
+                orderid as order_id,
+                max(created) as payment_finalized_date,
+                sum(amount) / 100.0 as total_amount_paid
+            from payments
+            where status <> 'fail'
+            group by 1
+        ) p on orders.id = p.order_id
+        left join customers as c on orders.user_id = c.id ),
 
-    ,customer_orders as (select 
-                            c.id as customer_id
-                            , min(order_date) as first_order_date
-                            , max(order_date) as most_recent_order_date
-                            , count(orders.id) as number_of_orders
-                        from customers c 
-                        left join {{ source('jaffle_shop', 'orders') }} as orders
-                        on orders.user_id = c.id 
-                        group by 1)
+    customer_orders as (
+        select 
+            c.id as customer_id
+            , min(order_date) as first_order_date
+            , max(order_date) as most_recent_order_date
+            , count(orders.id) as number_of_orders
+        from customers as c 
+        left join orders on orders.user_id = c.id 
+        group by 1
+    )
 
 select
     p.*,
@@ -48,12 +51,14 @@ select
     c.first_order_date as fdos
 from paid_orders p
 left join customer_orders as c using (customer_id)
-left outer join (select
-                    p.order_id,
-                    sum(t2.total_amount_paid) as clv_bad
-                from paid_orders p
-                left join paid_orders t2 on p.customer_id = t2.customer_id and p.order_id >= t2.order_id
-                group by 1
-                order by p.order_id
-                ) x on x.order_id = p.order_id
+left outer join 
+(
+    select
+        p.order_id,
+        sum(t2.total_amount_paid) as clv_bad
+    from paid_orders p
+    left join paid_orders t2 on p.customer_id = t2.customer_id and p.order_id >= t2.order_id
+    group by 1
+    order by p.order_id
+) x on x.order_id = p.order_id
 order by order_id
